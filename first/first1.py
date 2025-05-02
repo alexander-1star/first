eec3f3b304a0342d8c58d1d0600be10e166287da
alexander-1star <alexcruickshank2013@gmail.com>
2025-05-02 22:25:21 +0100

    helloo

--- first/first1.py
+++ first/first1.py
@@ -1,30 +1,8 @@
-import random
+print("please type a number:")
+low=int(input())
+print("Now type a bigger number:")
+high=int(input())
 
-class Student: 1 usage new
-
-def __init__(self, name):
-self.name = name
-self.gladness = 50
-self.progress = 0
-self.alive = True
-
-def to_study(self): 1 usage new
-print('Time to study')
-self.progress += 0.05
-self.gladness -= 0.01
-
-def to_sleep(self): 1 usage new
-print('I will sleep')
-self.gladness += 1.5
-
-def
-
-to_chill(self): 1 usage new
-print('Rest time')
-self.gladness += 0.4
-
-self.progress -= 0.1
-
-def
-
-is_alive(self): lusage new
+i=range(low,high)
+for n in i:
+    print(n)
