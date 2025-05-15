82d22af5bdda9a3678d810fde85a8ad6c168adb0
alexander-1star <alexcruickshank2013@gmail.com>
2025-05-15 19:56:27 +0100

    helloo

--- auth.py
+++ auth.py
@@ -1,5 +1,11 @@
-import tkinter
-import requests
+class car:
+    def __init__(self, brand, model, year):
+        self.brand = brand
+        self.model = model
+        self.year = year
+
+    def get_info(self):
+        return f"{self.year} {self.brand} {self.model}"
 
 
 
