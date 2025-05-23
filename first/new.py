dd05b5388f58d54c2d035a6bf6588f2ef33c89be
alexander-1star <alexcruickshank2013@gmail.com>
2025-05-23 10:18:07 +0100

    helloo

---
+++ first/new.py
@@ -0,0 +1,6 @@
+import colorama
+from colorama import Fore, Back, Style
+
+print(Fore.RED + 'hi') #text colour
+print(Back.GREEN + 'hi') #Backgroung colour
+print(Style.BRIGHT + 'hi')  #Makes text Bright
